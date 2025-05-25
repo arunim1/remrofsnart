ebd72662674f2329e20e465f8bcb1f5dec461eee
Arunim Agarwal <arunim.agarwal2@gmail.com>
2025-05-25 18:46:42 +0000

    wiki fwd

--- config/finetune_shakespeare.py
+++ config/finetune_shakespeare.py
@@ -1,21 +1,21 @@
 import time
 
-out_dir = 'out-rev-openwebtext'
+out_dir = 'out-wikipedia'
 eval_interval = 5
 eval_iters = 40
 wandb_log = True # feel free to turn on
-wandb_project = 'remrofsnart'
-wandb_run_name = 'gpu-rev-ft-openwebtext-' + str(time.time())
+wandb_projet = 'remrofsnart'
+wandb_run_name = 'gpu-ft-wikipedia-' + str(time.time())
 
-dataset = 'openwebtext'
-init_from = 'gpt2' # this is the largest GPT-2 model
+dataset = 'wikipedia'
+init_from = 'gpt2' 
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
 
 # the number of examples per iter:
 # 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
-# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
+# shakespeare has 55246589 tokens, so 1 epoch ~= 169 iters
 batch_size = 4
 gradient_accumulation_steps = 8
 max_iters = 200
--- data/wikipedia/prepare.py
+++ data/wikipedia/prepare.py
@@ -58,24 +58,31 @@ if __name__ == '__main__':
         num_proc=num_proc,
     )
 
-    # concatenate all the ids in each dataset into one large file we can use for training
     for split, dset in tokenized.items():
-        arr_len = np.sum(dset['len'], dtype=np.uint64)
-        filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
-        dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
-        arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
-        total_batches = 1024
+        arr_len = np.sum(dset["len"], dtype=np.uint64)
+        print(f"Total number of tokens in {split} split: {arr_len}")
+        filename = os.path.join(os.path.dirname(__file__), f"{split}.bin")
+        dtype = np.uint16                    # enc.max_token_value < 2**16
+        arr   = np.memmap(filename, dtype=dtype, mode="w+", shape=(arr_len,))
+
+        # never create more contiguous shards than there are rows
+        total_batches = min(1024, len(dset))    # <-- key line
 
         idx = 0
-        for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
-            # Batch together samples for faster write
-            batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
-            arr_batch = np.concatenate(batch['ids'])
-            # Write into mmap
+        for batch_idx in tqdm(range(total_batches), desc=f"writing {filename}"):
+            batch = (
+                dset
+                .shard(num_shards=total_batches, index=batch_idx, contiguous=True)
+                .with_format("numpy")
+            )
+            if len(batch) == 0:                 # guard against any empty shard
+                continue
+            arr_batch = np.concatenate(batch["ids"])
             arr[idx : idx + len(arr_batch)] = arr_batch
             idx += len(arr_batch)
-        arr.flush()
 
+        arr.flush()
+    
     # train.bin and val.bin sizes will vary depending on the wikipedia dataset chosen
     # Simple English Wikipedia is much smaller than full Wikipedia
 
